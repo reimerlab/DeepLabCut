14ddd5322e1c8eedae29d47712f744084031ee96
Donnie Kim <kdk411@gmail.com>
2019-03-13 13:57:59 -0500

    add connect_eyelids and fit_circle_to_pupil methods

--- DLC_tool_DK/plotting_tools.py
+++ DLC_tool_DK/plotting_tools.py
@@ -12,6 +12,7 @@ import time
 import imageio
 from collections import OrderedDict
 from itertools import cycle
+from DLC_tool_DK.cropping_tool import update_config_crop_coords
 
 from deeplabcut.utils import auxiliaryfunctions
 from deeplabcut.utils.plotting import get_cmap
@@ -21,7 +22,6 @@ from deeplabcut.utils.video_processor import VideoProcessorCV as vp
 # for ipython purpose
 import pylab as pl
 from IPython import display
-
 
 def get_frame(path_to_video, frame_num):
     cap = cv2.VideoCapture(path_to_video)
@@ -106,13 +106,13 @@ class PlotBodyparts():
         self.shuffle = shuffle
         self.trainingsetindex = trainingsetindex
 
-        case_full_name = case + '_beh'
-        project_path = self.config['project_path']
-        path_to_video = os.path.join(
-            project_path, 'videos', case_full_name + '.avi')
+        self._case_full_name = case + '_beh'
+        self.project_path = self.config['project_path']
+        self.path_to_video = os.path.join(
+            self.project_path, 'videos', self._case_full_name + '.avi')
         self.label_path = os.path.join(
-            project_path, 'analysis', case_full_name)
-        self.clip = vp(fname=path_to_video)
+            self.project_path, 'analysis', self._case_full_name)
+        self.clip = vp(fname=self.path_to_video)
 
         # plotting properties
         self._dotsize = 5
@@ -209,11 +209,11 @@ class PlotBodyparts():
     def cropping_coords(self):
         return self._cropping_coords
 
-    # we need to link with cropping tool
+    #TODO link cropping and cropping_coords with cropping tool
     @cropping_coords.setter
     def cropping_coords(self, value):
-        if self._cropping:
-            if ~isinstance(value, list):
+        if self.cropping:
+            if not isinstance(value, list):
                 raise TypeError("coordinates must be a list")
             elif len(value) != 4:
                 raise ValueError("length of the coordinates must be 4")
@@ -221,6 +221,40 @@ class PlotBodyparts():
         else:
             print("Cropping is set to False! You cannot reset the cropping coordinates. Default value at original frame size")
 
+    @property
+    def bodyparts_info(self):
+        return self._df_bodyparts_likelihood, self._df_bodyparts_x, self._df_bodyparts_y
+    
+    @bodyparts_info.setter
+    def bodyparts_info(self):
+        """
+        Given bodyparts, return corresponding likelihood, x-coordinates, and y-coordinates in dataframe
+
+        Using pandas instead of numpy as my data is in range of 50k to 500k
+        http://gouthamanbalaraman.com/blog/numpy-vs-pandas-comparison.html
+        """
+        trainFraction = config['TrainingFraction'][self.trainingsetindex]
+        DLCscorer = auxiliaryfunctions.GetScorerName(
+            self.config, self.shuffle, trainFraction)
+
+        df_label = pd.read_hdf(os.path.join(
+            self.label_path, self._case_full_name + DLCscorer + '.h5'))
+
+        df_bodyparts = df_label[DLCscorer][bodyparts]
+        nframes = df_bodyparts.shape[0]
+
+        df_bodyparts_likelihood = df_bodyparts.iloc[:, df_bodyparts.columns.get_level_values(
+            1) == 'likelihood']
+        df_bodyparts_x = df_bodyparts.iloc[:,
+                                        df_bodyparts.columns.get_level_values(1) == 'x']
+        df_bodyparts_y = df_bodyparts.iloc[:,
+                                        df_bodyparts.columns.get_level_values(1) == 'y']
+        if self.cropping:
+            df_bodyparts_x -= self.cropping_coords[0]
+            df_bodyparts_y -= self.cropping_coords[2]
+        
+        return df_bodyparts_likelihood, df_bodyparts_x, df_bodyparts_y
+        
     def coords_pcutoff(self, frame_num):
         """
         Given a frame number, return bpindex, x & y coordinates that meet pcutoff criteria
@@ -247,7 +281,7 @@ class PlotBodyparts():
     def plot_one_frame(self, frame_num, save_fig=False, save_gif=False):
 
         plt.axis('off')
-        fig = plt.figure(frameon=False, figsize=(12, 8))
+        fig = plt.figure(frameon=False, figsize=(10, 8))
         # fig = plt.figure(frameon=False, figsize=(nx * 1. / 100, ny * 1. / 100))
         plt.subplots_adjust(left=0, bottom=0, right=1,
                             top=1, wspace=0, hspace=0)
@@ -298,16 +332,20 @@ class PlotBodyparts():
             fig.canvas.draw()
             image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
             image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-            plt.close('all')
+            
             return image
 
-        plt.close('all')
+        # plt.show()
+        # plt.close('all')
+        
         return fig
 
     def plot_over_frames(self, start, end, save_fig=False):
 
         for i in range(start, end):
             self.plot_one_frame(i, save_fig=save_fig)
+        
+        # plt.close('all')
 
     def make_gif(self, start, num_frames, save_gif=True):
         gif_name = self.case + '_' + \
@@ -315,7 +353,7 @@ class PlotBodyparts():
         save_dir = os.path.join(
             self.config['project_path'], 'analysis', self.case + '_beh', gif_name)
 
-        plt.figure(frameon=False, figsize=(12, 8))
+        plt.figure(frameon=False, figsize=(10, 8))
         plt_list = []
 
         for i in range(start, start + num_frames):
@@ -323,9 +361,9 @@ class PlotBodyparts():
             plt_list.append(plot)
         imageio.mimsave(save_dir, plt_list, fps=1)
 
-# for this class, all bodyparts must be provided... so why bother providing bodyparts as input?
-class PupilFitting(PlotBodyparts):
 
+class PupilFitting(PlotBodyparts):
+    # for this class, all bodyparts must be provided... so why bother providing bodyparts as input?
     def __init__(self, path_to_config, case, bodyparts, trainingsetindex=0, shuffle=1):
         super().__init__(path_to_config, case, bodyparts, trainingsetindex=0, shuffle=1)
 
@@ -343,9 +381,6 @@ class PupilFitting(PlotBodyparts):
                                       'eyelid_left_top': 'eyelid_top'}
 
     def old_plot_fitted_frame(self, frame_num, save_fig=False, save_gif=False):
-        pass
-
-    def connect_eyelids(self, frame_num):
         pass
 
     def coords_pcutoff(self, frame_num):
@@ -371,30 +406,62 @@ class PupilFitting(PlotBodyparts):
 
         return bpindex, df_x_coords, df_y_coords
 
+    def connect_eyelids(self, frame_num, frame):
+        """
+        connect eyelid labels with a straight line. If a label is missing, do not connect and skip to the next label.
+        Input:
+            frame_num: int
+                A desired frame number
+            frame: numpy array
+                A frame to be fitted
+        Output:
+            frame:
+                A numpy array frame with eyelids connected
+        """
+
+        _, df_x_coords, df_y_coords = self.coords_pcutoff(frame_num)
+        eyelid_labels = [label for label in list(
+            df_x_coords.index.get_level_values(0)) if 'eyelid' in label]
+
+        for eyelid in eyelid_labels:
+            next_bp = self.complete_eyelid_graph[eyelid]
+
+            if next_bp not in eyelid_labels:
+                continue
+
+            coord_0 = tuple(
+                map(int, map(round, [df_x_coords[eyelid].values[0], df_y_coords[eyelid].values[0]])))
+            coord_1 = tuple(
+                map(int, map(round, [df_x_coords[next_bp].values[0], df_y_coords[next_bp].values[0]])))
+            # opencv has some issues with dealing with np objects. Cast it manually again
+            frame = cv2.line(
+                np.array(frame), coord_0, coord_1, color=(0, 255, 0), thickness=self.line_thickness)
+
+        return frame
+
     def fit_circle_to_pupil(self, frame_num, frame):
         """
         Fit a circle to the pupil
         Input:
-            frame_num: 
+            frame_num: int
                 A desired frame number
-            frame:
+            frame: numpy array
                 A frame to be fitted
-        Output:
+        Output: dictionary
             A dictionary with the fitted frame, center and radius of the fitted circle. If fitting did
             not occur, return the original frame with center and raidus as None
         """
 
         _, df_x_coords, df_y_coords = self.coords_pcutoff(frame_num)
 
-        pupil_index = []
-        pupil_labels = []
-        for ind,label in enumerate(list(df_x_coords.index.get_level_values(0))):
-            if label.startswith('pupil'):
-                pupil_index.append(ind)
-                pupil_labels.append(label)
+        pupil_labels = [label for label in list(
+            df_x_coords.index.get_level_values(0)) if 'pupil' in label]
+
+        print(pupil_labels)
 
         if len(pupil_labels) <= 1:
-            print('Frame number: {} has only 1 or less pupil label. Skip fitting!'.format(frame_num))
+            print('Frame number: {} has only 1 or less pupil label. Skip fitting!'.format(
+                frame_num))
             center = None
             radius = None
 
@@ -402,29 +469,29 @@ class PupilFitting(PlotBodyparts):
             print('havent implemented yet!')
             center = None
             radius = None
-            return frame
 
-        elif len(pupil_labels) >=3:
+        elif len(pupil_labels) >= 3:
             pupil_x = df_x_coords.loc[pupil_labels].values
             pupil_y = df_y_coords.loc[pupil_labels].values
-            
-            pupil_coords = np.array([pupil_x,pupil_y]).reshape(-1, 1, 2).astype(int)
+
+            pupil_coords = np.array(zip)
+
+            pupil_coords = np.array(
+                list(zip(pupil_x, pupil_y))).reshape(-1, 1, 2).astype(int)
+
             (x, y), radius = cv2.minEnclosingCircle(pupil_coords)
             center = (int(x), int(y))
             radius = int(radius)
             # opencv has some issues with dealing with np objects. Cast it manually again
             frame = cv2.circle(np.array(frame), center,
                                radius, color=(0, 255, 0), thickness=self.line_thickness)
-            
 
-        return {'frame':frame, 'center': center, 'radius':radius}
-
-
+        return {'frame': frame, 'center': center, 'radius': radius}
 
     def plot_fitted_frame(self, frame_num, save_fig=False, save_gif=False):
 
         plt.axis('off')
-        fig = plt.figure(frameon=False, figsize=(12, 8))
+        fig = plt.figure(frameon=False, figsize=(10, 8))
         # fig = plt.figure(frameon=False, figsize=(nx * 1. / 100, ny * 1. / 100))
         plt.subplots_adjust(left=0, bottom=0, right=1,
                             top=1, wspace=0, hspace=0)
@@ -448,47 +515,12 @@ class PupilFitting(PlotBodyparts):
         plt.scatter(x_coords.values, y_coords.values, s=self.dotsize**2,
                     color=self._label_colors(bpindex), alpha=self.alphavalue)
 
-        circle_dict = self.fit_circle_to_pupil(frame_num, frame = image)
+        eyelid_connected_frame = self.connect_eyelids(frame_num, frame=image)
 
-        for bpindex, bp in enumerate(self.bodyparts):
-            if self.df_bodyparts_likelihood[bpindex, frame_num] > self.pcutoff:
-                plt.scatter(
-                    self.df_bodyparts_x[bpindex, frame_num],
-                    self.df_bodyparts_y[bpindex, frame_num],
-                    s=self.dotsize**2,
-                    color=self._label_colors(bpindex),
-                    alpha=self.alphavalue
-                )
-                if bp in self.pupil_parts:
-                    pupil_coords.append(
-                        [self.df_bodyparts_x[bpindex, frame_num], self.df_bodyparts_y[bpindex, frame_num]])
+        circle_fit = self.fit_circle_to_pupil(
+            frame_num, frame=eyelid_connected_frame)
 
-                if bp in self.eyelid_parts:
-                    eyelid_coords[bp] = [
-                        [self.df_bodyparts_x[bpindex, frame_num], self.df_bodyparts_y[bpindex, frame_num]]]
-
-        if len(pupil_coords) >= 3:
-            pupil_coords = np.array(pupil_coords).reshape(-1, 1, 2).astype(int)
-            (x, y), radius = cv2.minEnclosingCircle(pupil_coords)
-            center = (int(x), int(y))
-            radius = int(radius)
-            # opencv has some issues with dealing with np objects. Cast it manually again
-            frame = cv2.circle(np.array(image), center,
-                               radius, color=(0, 255, 0), thickness=self.line_thickness)
-
-        for bp, coords in eyelid_coords.items():
-            next_bp = self.complete_eyelid_graph[bp]
-
-            if next_bp not in eyelid_coords.keys():
-                continue
-            # opencv takes coords in tuple of ints. Convert the coords first
-            coord_0 = tuple(map(int, (map(round, *coords))))
-            coord_1 = tuple(map(int, (map(round, *eyelid_coords[next_bp]))))
-            frame = cv2.line(
-                frame, coord_0, coord_1, color=(0, 255, 0), thickness=self.line_thickness)
-
-        plt.imshow(frame)
-
+        plt.imshow(circle_fit['frame'])
         plt.xlim(0, self.nx)
         plt.ylim(0, self.ny)
         plt.axis('off')
@@ -520,5 +552,5 @@ class PupilFitting(PlotBodyparts):
             plt.close('all')
             return image
 
-        plt.close('all')
+        # plt.close('all')
         return fig
